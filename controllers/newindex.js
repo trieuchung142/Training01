7c38ef8f9b8ac281f34ffad2bad22ef306e3cf13
Chung Trieu <trieuchung142@hotmail.com>
2017-03-03 22:01:43 +0700

    add show data

--- controllers/newindex.js
+++ controllers/newindex.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
+// router.route('/')
+// 	.get(function(req, res){
+// 		res.render('index')
+// 	})
 router.route('/')
 	.get(function(req, res){
-		res.render('index')
+		var option = {
+			url: "http://localhost:5000/api/user",
+			method: 'get'
+		};
+		request(option, function (error, response, list_user) {
+			if(!error && response.statusCode == 200){
+				res.render('showconfirm',{
+					user: JSON.parse(list_user).data
+				})	
+			}
+		})
+		
 	})
-
  module.exports = router;
